6d81b21a9501e415670476d1d0c0fe2887bfa394
John J Hayes <34247425+ChefKef@users.noreply.github.com>
2022-02-10 17:05:53 -0500

    Create Initial Board
    Created an initial board with completed board objects placed into it. Fixed bugs with bumpers having no velocity cap, slingshots no launching the ball correctly when scaled negatively, and slingshots not having proper collision.

--- Power Pinball/Assets/Scripts/John/Bumper.cs
+++ Power Pinball/Assets/Scripts/John/Bumper.cs
@@ -7,7 +7,7 @@ public class Bumper : MonoBehaviour
     private CircleCollider2D hitReg;
     public float elasticity = 5f; //How much rebound a shot will have when hitting the bumper.
     public float minimumLaunch = 50f; //The minimum amount of force with witch the ball will be launched after hitting the bumper
-    // Start is called before the first frame update
+    public float maximumLaunch = 600f; //Used to keep the ball from clipping out of bounds. Change only with exstensive testing. Going over 600 is asking for trouble.
     void Start()
     {
         hitReg = GetComponent<CircleCollider2D>();
@@ -30,6 +30,11 @@ public class Bumper : MonoBehaviour
             {
                 launchMagnitude = minimumLaunch;
             }
+            if(launchMagnitude > maximumLaunch)
+            {
+                launchMagnitude = maximumLaunch;
+            }
+            Debug.Log("Launch magnitude: " + launchMagnitude);
             Vector3 ballPos = collision.gameObject.transform.position;
             Vector3 bumperPos = gameObject.transform.position;
             Vector2 ballDir = new Vector2(ballPos.x - bumperPos.x, ballPos.y - bumperPos.y);
--- Power Pinball/Assets/Scripts/John/Slingshot.cs
+++ Power Pinball/Assets/Scripts/John/Slingshot.cs
@@ -8,9 +8,14 @@ public class Slingshot : MonoBehaviour
     public float elasticity = 2.5f; //How much rebound a shot will have when hitting the slingshot.
     public float minimumLaunch = 30f; //The minimum amount of force with which the ball will be launched after hitting the slingshot.
     public float angleManipulation = 1.2f; //How much the angle is corrected. Closer to 1 is full angle correction, higher number = wilder launches.
+    private bool isFlipped = false;
     void Start()
     {
         hitReg = GetComponent<EdgeCollider2D>();
+        if (transform.localScale.y < 0) //If slingshot is flipped, mark it as so, and alter it's up vector accordingly.
+        {
+            isFlipped = true;
+        }
     }
 
     // Update is called once per frame
@@ -21,7 +26,7 @@ public class Slingshot : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<PinballManager>())
+        if (collision.gameObject.GetComponent<PinballManager>() && hitReg.IsTouching(collision.gameObject.GetComponent<CircleCollider2D>()))
         {
             PinballManager ballsManager = collision.gameObject.GetComponent<PinballManager>();
             float launchMagnitude = ballsManager.movementMagnitue();
@@ -37,6 +42,11 @@ public class Slingshot : MonoBehaviour
             Debug.Log("Launch direction:" + ballDir.x + ", " + ballDir.y);
             Debug.Log("Transform up: " + transform.up);
             Vector2 slingshotUp = transform.up.normalized;
+            if(isFlipped)
+            {
+                slingshotUp.x = -slingshotUp.x;
+                slingshotUp.y = -slingshotUp.y;
+            }
             ballDir = new Vector2(ballDir.x + (slingshotUp.x - ballDir.x) / angleManipulation, ballDir.y + (slingshotUp.y - ballDir.y) / angleManipulation);
             ballDir *= launchMagnitude;
             ballsManager.setVelocity(ballDir);
