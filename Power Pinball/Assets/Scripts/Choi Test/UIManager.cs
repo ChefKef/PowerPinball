c04122a7cac1067be997dad36b3dd6019e6e69e3
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2022-02-16 19:19:04 -0500

    Finished adding score to gameplay screen.

--- Power Pinball/Assets/Scripts/Choi Test/UIManager.cs
+++ Power Pinball/Assets/Scripts/Choi Test/UIManager.cs
@@ -8,10 +8,26 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI scoreText;
 
+    /// <summary>
+    /// How far to push the TMP rectangle right.
+    /// </summary>
+    [SerializeField]
+    private int leftMargin;
+
+    /// <summary>
+    /// How far to push the TMP rectangle up.
+    /// </summary>
+    [SerializeField]
+    private int bottomMargin;
+
     // Start is called before the first frame update
     void Start()
     {
-        scoreText.transform.position = new Vector3(50, 50);
+        // Leverage width and height of the TMP's rectangle to ensure it fits
+        // wholly onscreen.
+        scoreText.rectTransform.position = new Vector3(
+            scoreText.rectTransform.rect.width + leftMargin, 
+            scoreText.rectTransform.rect.height + bottomMargin);
     }
 
     // Update is called once per frame
