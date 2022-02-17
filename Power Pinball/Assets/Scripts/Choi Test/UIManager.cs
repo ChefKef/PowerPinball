d169b4b866e60f4cdade319599a401a90ac846bb
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2022-02-16 19:49:09 -0500

    Added a round timer at the top of the gameplay screen.

--- Power Pinball/Assets/Scripts/Choi Test/UIManager.cs
+++ Power Pinball/Assets/Scripts/Choi Test/UIManager.cs
@@ -8,6 +8,9 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI scoreText;
 
+    [SerializeField]
+    private TextMeshProUGUI roundTimerText;
+
     /// <summary>
     /// How far to push the TMP rectangle right.
     /// </summary>
@@ -20,19 +23,39 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private int bottomMargin;
 
+    [SerializeField]
+    private int roundLength;
+
+    private float roundTimer;
+
     // Start is called before the first frame update
     void Start()
     {
-        // Leverage width and height of the TMP's rectangle to ensure it fits
+        // Leverage width and height of scoreText's rectangle to ensure it fits
         // wholly onscreen.
         scoreText.rectTransform.position = new Vector3(
             scoreText.rectTransform.rect.width + leftMargin, 
             scoreText.rectTransform.rect.height + bottomMargin);
+
+        // Do the same for the round timer.
+        // Transforms of TMP's are at the centre of the textbox, not the top
+        // left corner!
+        roundTimerText.rectTransform.position = new Vector3(
+            Screen.width / 2,
+            Screen.height - roundTimerText.rectTransform.rect.height / 2);
+
+        roundTimer = roundLength;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Don't let the timer display negative values.
+        if (roundTimer > 0) roundTimer -= Time.deltaTime;
+
         scoreText.text = "Score: " + GameManager.scoreP1;
+
+        // Only show the remaining time as an integer.
+        roundTimerText.text = ((int)roundTimer).ToString();
     }
 }
