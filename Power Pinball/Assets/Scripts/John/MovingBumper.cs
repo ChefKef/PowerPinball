8c60d372e53d9507f4771cef07adda4472cdf12d
John J Hayes <34247425+ChefKef@users.noreply.github.com>
2022-04-14 19:34:56 -0400

    Board First final
    Added kickout hole to board, added drop targets to board, created and added moving bumper to board. Fixed board border color.
    This is the first finalized layout of the board, but the kickout hole, knockdowns, and moving bumper all need functionality.

--- Power Pinball/Assets/Scripts/John/MovingBumper.cs
+++ Power Pinball/Assets/Scripts/John/MovingBumper.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MovingBumper : Bumper
 {
+    public float radius = 1f;
+    public float period = 2f; //How long, in seconds, it takes the bumper to make a full rotation.
+
+    private Vector3 anchor;
+    private float timer = 0f;
+    private float deg = 0f;
     // Start is called before the first frame update
     void Start()
     {
- 
+        anchor = transform.position;
+        transform.position = new Vector3(anchor.x + (Mathf.Sin(Mathf.Deg2Rad * deg) * radius), anchor.y + (Mathf.Cos(Mathf.Deg2Rad * deg) * radius), anchor.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        deg = timer / period * 360f;
+        transform.position = new Vector3(anchor.x + (Mathf.Sin(Mathf.Deg2Rad * deg) * radius), anchor.y + (Mathf.Cos(Mathf.Deg2Rad * deg) * radius), anchor.z);
     }
 }
